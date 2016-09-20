daa34fc1450d8d21f6f3144069e8ca871ac1425a
hitori <hitori.achmad@viva.co.id>
2016-09-20 20:49:39 +0700

    Infinite Scroll OK

--- resource/js/views/Terbaru.js
+++ resource/js/views/Terbaru.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Moment from 'moment';
 import {Card,
         CardActions,
         CardHeader,
@@ -8,14 +9,18 @@ import {Card,
 import LazyLoad from 'react-lazy-load';
 import Divider from 'material-ui/Divider';
 import RefreshIndicator from 'material-ui/RefreshIndicator';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import axios from 'axios';
 
 export default class Content extends React.Component{
   constructor(){
+    Moment.locale("id");
     super();
     this.state = {
       terbaru : null
+      ,last_publish_date : null
+      ,allowLoad : true
     }
   }
   componentDidMount(){
@@ -26,21 +31,77 @@ export default class Content extends React.Component{
       _this.setState({
         terbaru : result.data.response[0]['terbaru']
       });
-    })
+    }).then(() => {
+      var contentLength = _this.state.terbaru.length;
+      var lpd = _this.state.terbaru[contentLength - 1]['date_publish'];
+      _this.setState({
+        last_publish_date : lpd
+      });
+    });
+    var k = 0;
+    window.onscroll = function(){
+      var topOffset = document.documentElement.scrollTop || document.body.scrollTop;
+      var wHeight = window.innerHeight;
+
+      var body = document.body,
+          html = document.documentElement;
+
+      var areaHeight = Math.max( body.scrollHeight, body.offsetHeight,
+                             html.clientHeight, html.scrollHeight, html.offsetHeight );
+      var bottomClient = topOffset + wHeight;
+      var load_more_offset = areaHeight - 100;
+      var allowLoad = _this.state.allowLoad;
+      if(load_more_offset <= bottomClient && allowLoad){
+        _this.lanjut();
+        console.log("OK NIH");
+        _this.setState({
+          allowLoad : false
+        })
+        allowLoad = false;
+      }
+    }
+  }
+  lanjut(){
+    var _this = this;
+    var pageStart = this.state.terbaru.length;
+    var published = this.state.last_publish_date;
+    published = Moment(published).format("X");
+    this.serverRequest = axios.get(
+      'http://api.viva.co.id/v/901/terbarulist/published/' + published
+    ).then((result) => {
+      console.log("Lanjut");
+      var newResult = result.data.response[0]['terbaru'];
+      _this.setState({
+        terbaru : this.state.terbaru.concat(newResult)
+      });
+      //_this.forceUpdate();
+    }).then(() => {
+      var contentLength = _this.state.terbaru.length;
+      var lpd = _this.state.terbaru[contentLength - 1]['date_publish'];
+      _this.setState({
+        last_publish_date : lpd
+      });
+    }).then(()=>{
+      _this.setState({
+        allowLoad : true
+      });
+    });
   }
   actionTap(x){
 
   }
   cardLoad(object,i){
+    var tanggal = Moment(object.date_publish)
+                  .format("ddd, DD MMM YYYY - HH:mm", "id");
     return (
       <div>
         <Card style={{marginBottom : '10px'}}>
-          <LazyLoad>
+          <LazyLoad offset={1000}>
             <CardMedia>
               <img src={object.image_url} />
             </CardMedia>
           </LazyLoad>
-          <CardTitle>{object.title}</CardTitle>
+          <CardTitle subtitle={tanggal} title={object.title}></CardTitle>
         </Card>
       </div>
 
@@ -62,7 +123,8 @@ export default class Content extends React.Component{
       );
     }
     var _this = this;
-    const {terbaru} = this.state;
+    const terbaru = this.state.terbaru;
+    const last_publish_date = this.state.last_publish_date;
     return(
       <div style={{paddingTop : '70px'}}>
         {
@@ -73,6 +135,7 @@ export default class Content extends React.Component{
             </div>
           )
         })}
+        <RaisedButton onTouchTap={this.lanjut.bind(this)} label="Selanjutnya ..." fullWidth={true} />
       </div>
     );
   }
